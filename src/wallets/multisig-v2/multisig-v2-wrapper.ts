9f5c3c9a050505afe79f6ea5430e91574e4a2cb7
ipromise2324 <168allen55@gmail.com>
2025-05-19 19:45:11 +0800

    refactor: simplify variable declaration in getOrder method
    - Changed the `lib_prep` variable from `let` to `const` for better readability and to reflect its immutability.

--- src/wallets/multisig-v2/multisig-v2-wrapper.ts
+++ src/wallets/multisig-v2/multisig-v2-wrapper.ts
@@ -265,7 +265,7 @@ export class Multisig implements Contract {
 
   getOrder(_: ContractProvider, orderSeqno: bigint): OrderContract {
     const code_raw = Cell.fromHex(ORDER_CODE);
-    let lib_prep = beginCell().storeUint(2, 8).storeBuffer(code_raw.hash()).endCell();
+    const lib_prep = beginCell().storeUint(2, 8).storeBuffer(code_raw.hash()).endCell();
     const code = new Cell({ exotic: true, bits: lib_prep.bits, refs: lib_prep.refs });
     const data = orderConfigToCell({ multisig: this.address, orderSeqno });
     const init = { code, data };
